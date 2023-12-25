b45c549c1b73133569395337c69832e59724e84b
unknown <psajeedran@gmail.com>
2023-12-25 13:56:20 +0530

    'init'

---
+++ justlisted-ai-backend-main/model/premade.model.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const PreMadeSchema = new mongoose.Schema(
+    {
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            require: true
+        },
+        propertyName: {
+            type: String,
+            required: true,
+        },
+        premadeList: {
+            type: String,
+            required: true,
+        },
+        textLimit: {
+            type: Number,
+            required: true,
+        },
+        language: {
+            type: String,
+            required: true,
+        },
+        generateSummary: {
+            type: String,
+        },
+        regenerateSummary: {
+            type: String,
+        },
+        socialMediaCaption: {
+            type: String,
+        },
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("PreMade", PreMadeSchema);
